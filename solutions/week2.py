a5257830038f7a8f393e57de9c308c6118158205
Jackson Gatenby <jackson.gatenby@gmail.com>
2013-12-05 14:28:36 +1000

    an extra "leap years" task for week 2

--- solutions/week2.py
+++ solutions/week2.py
@@ -9,6 +9,22 @@ print "Hello, " + name + "!"
 
 # Task 2
 
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+# Probably harder to interpret, but significantly shorter
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+# Task 3
+
 import random
 a = random.randint(10, 99)
 b = random.randint(10, 99)
