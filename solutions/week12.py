39b32286aef5b56ee5d0694968ce0a1e50b1e03e
Jackson Gatenby <jackson.gatenby@gmail.com>
2014-06-17 17:02:29 +1000

    Finish tweaking the timing on week12 solutions

--- solutions/week12.py
+++ solutions/week12.py
@@ -15,32 +15,31 @@ def uniqify2(items):
 
 
 def uniqify3(items):
-    return dict.fromkeys(items).keys()
+    d = {}
+    for x in xs:
+        d[x] = None
+    return d.keys()
 
-#print uniqify([8, 3, 5, 8, 9, 4, 2, 5, 3, 5])
-#print uniqify2([8, 3, 5, 8, 9, 4, 2, 5, 3, 5])
-#print uniqify3([8, 3, 5, 8, 9, 4, 2, 5, 3, 5])
-
-import random
-import timeit
-
-# TODO: work in progress - calibrate parameters
+    ## This can be done in one line:
+    # return dict.fromkeys(items).keys()
 
 
-def test(function_name, size, repeat, number):
-    stmt = "{0}(xrange({1}))".format(function_name, size)
-    setup = "from __main__ import {0}".format(function_name)
-    return min(timeit.repeat(stmt, setup, repeat=repeat,
-                             number=number)) / number
+if __name__ == '__main__':
+    #print uniqify([8, 3, 5, 8, 9, 4, 2, 5, 3, 5])
+    #print uniqify2([8, 3, 5, 8, 9, 4, 2, 5, 3, 5])
+    #print uniqify3([8, 3, 5, 8, 9, 4, 2, 5, 3, 5])
 
-print test('uniqify', 5000, 10, 1)
-print test('uniqify', 10000, 10, 1)
-print test('uniqify', 20000, 10, 1)
+    # Run this at the beginning of the tutorial hour.
+    # The N = 10**6 case will not finish in time
 
-print test('uniqify2', 50000, 10, 1)
-print test('uniqify2', 100000, 10, 1)
-print test('uniqify2', 200000, 10, 1)
+    import random
+    import timeit
 
-print test('uniqify3', 50000, 10, 1)
-print test('uniqify3', 100000, 10, 1)
-print test('uniqify3', 200000, 10, 1)
+    for N in [1000, 10000, 100000, 1000000]:
+        items = [random.randint(0,N) for i in xrange(N)]
+        print "n =", N
+        print "uniqify3:", min(timeit.repeat('uniqify3(items)', 'from __main__ import uniqify3, items', number=100))/100, 'seconds'
+        print "uniqify2:", min(timeit.repeat('uniqify2(items)', 'from __main__ import uniqify2, items', number=100))/100, 'seconds'
+        print "uniqify:", min(timeit.repeat('uniqify(items)', 'from __main__ import uniqify, items', number=1)), 'seconds'
+        print
+
